820c564876635f24e5677cc35b456bfb47914d61
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-28 21:13:12 +0200

    updated todo

---
+++ TODO.js
@@ -0,0 +1,5 @@
+// TODO: Header
+// TODO: Function drag and drop div
+// TODO: Randomize numbers for width and height
+// TODO: Get height and width for guess
+// TODO: Compare guess with task numbers and calculate score

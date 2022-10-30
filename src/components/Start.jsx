9c26b3ccacd9507591899e39c1659ae8065f517a
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-30 07:05:07 +0100

    basic start page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -8,11 +8,17 @@ export const Start = () => {
     <div className="start">
       <div className="start--content">
         <Title />
-        <p>
-          Welcome to Pixactly. An online tool that tests how well you
-          know your pixels. Draw a box that matches the dimensions
-          given and receive a score based on how close you are. The
-          lower the score... the better! Sounds easy, right? Pixactly.
+        <p className="start--content--text">
+          Welcome to Pixactly.
+          <br />
+          An online tool that tests how well you know your pixels.
+          <br />
+          <br />
+          Draw a box that matches the dimensions given and receive a
+          score based on how close you are. The lower the score... the
+          better! Sounds easy, right?
+          <br />
+          Pixactly.
         </p>
         <Btn>Start Game</Btn>
       </div>

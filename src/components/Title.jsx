abb1dde53e3046807759735adeaee5ac47cbc0d8
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-30 16:00:48 +0100

    added title animation on first page

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,5 +4,7 @@ import { GameContext } from "../contexts/GameContext"
 export const Title = () => {
   const { gameState } = useContext(GameContext)
   const { started } = gameState
-  return <h1 className={`title ${started && "top"}`}>PxlGuessr</h1>
+  return (
+    <h1 className={`title stretch ${started && "top"}`}>PxlGuessr</h1>
+  )
 }

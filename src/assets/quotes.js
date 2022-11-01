f239fac491cc7c7d97e662d9084172d397454114
jeppe314 <jesperlindqvist93@gmail.com>
2022-11-01 12:15:58 +0100

    moved quotes to separate file

---
+++ src/assets/quotes.js
@@ -0,0 +1,16 @@
+export const feedbackQuotes = {
+  round: {
+    1: "WOW!! That was inch perfect!",
+    2: "Very impressive! That was a great guess!",
+    3: "Good guess!",
+    4: "That was... below par.",
+    5: "Come on... Can you atleast try?",
+  },
+  post: {
+    1: "Teach me, master. You truly are pixel perfect!",
+    2: "You're up there with the best!",
+    3: "You're bang on average!",
+    4: "Just don't quit your day job.",
+    5: "That was an all around horrific experience. Try again and improve your pixel predictions.",
+  },
+}
--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from "react"
+import { feedbackQuotes } from "../assets/quotes"
 
 export const GameContext = createContext()
-
 export const GameContextProvider = ({ children }) => {
   const [gameState, setGameState] = useState({
     finished: false,
@@ -18,23 +18,6 @@ export const GameContextProvider = ({ children }) => {
     roundScores: [],
     score: 0,
   })
-
-  const feedbackQuotes = {
-    round: {
-      1: "WOW!! That was inch perfect!",
-      2: "Very impressive! That was a great guess!",
-      3: "Good guess!",
-      4: "That was... below par.",
-      5: "Come on... Can you atleast try?",
-    },
-    post: {
-      1: "Teach me, master. You truly are pixel perfect!",
-      2: "You're up there with the best!",
-      3: "You're bang on average!",
-      4: "Just don't quit your day job.",
-      5: "That was an all around horrific experience. Try again and improve your pixel predictions.",
-    },
-  }
 
   const { guessed } = gameState
 

9040902b78c265eae745b6e467eea811e02f7531
jeppe314 <jesperlindqvist93@gmail.com>
2022-11-02 10:31:44 +0100

    feat highscore component

---
+++ src/components/HighScore.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const HighScore = () => {
+  return (
+    <div className="highscore">
+      <h1>High Scores</h1>
+      <h2>Top 100</h2>
+      <ul className="highscore--list">
+        <li>1. Jesper...... 173%</li>
+        <li>1. Jesper...... 173%</li>
+        <li>1. Jesper...... 173%</li>
+        <li>1. Jesper...... 173%</li>
+        <li>1. Jesper...... 173%</li>
+      </ul>
+    </div>
+  )
+}

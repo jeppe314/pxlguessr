044b8b9d1afd54b162a977236445384dcbd86fc8
jeppe314 <jesperlindqvist93@gmail.com>
2022-11-09 18:59:58 +0100

    add future implementations to todo

--- TODO.js
+++ TODO.js
@@ -4,6 +4,8 @@
 // CHECK: Get height and width for guess
 // CHECK: Compare guess with task numbers and calculate
 
+const { ClimbingBoxLoader } = require("react-spinners")
+
 // CHECK: Box flickers and resizes double size on mousemove?
 
 // CHECK: Fix feedback quotes on feedback and result
@@ -14,8 +16,16 @@
 // TODO: Timestamp on score in firestore to set date in highscore list
 // CHECK: Update start title animation to look like resizing box
 // CHECK: Loading animation on highscore
-// TODO: IN PROGRESS Create reducer?
+// CHECK: IN PROGRESS Create reducer?
 
 // TODO: Create different context for pauses, modals and such?
 // CHECK: Conditional rendering of blur when modal is open
 // CHECK: Home function messes up game
+
+// ADDITIONAL FEATURES TO IMPLEMENT IN FUTURE WHEN NOTHING ELSE TODO:
+
+// RATIO POINTS (WIDTH/HEIGHT) * 100 TO MEASURE THE SHAPE OF THE BOX => CAN SAVE POINTS IF ITS THE RIGHT SIZE
+// AUTOMATICALLY SCROLL TO PLAYER SCORE IN HIGHSCORE LIST
+// CREATE BOXES IN BACKGROUND ON START PAGE
+// DIFFERENT GAME MODES (GUESS DIMENSIONS OF GIVEN BOX, CHOOSE NUMBER OF ROUNDS)
+// HIGHSCORE THIS WEEK, THIS MONTH, THIS YEAR, ALL TIME
